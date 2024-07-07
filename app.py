fd563ca9a0ef91f975595fe6f03c01d75b546ce1
VTS072Varun <varun@vivatechsolutions.io>
2024-07-07 23:33:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__)  # Correct use of __name_
 
+@app.route('/test', methods=['POST'])
+def run():
+    return '{"message":"hello python"}'  # Correct JSON format
 
-@app.route("/")
-def hello_world():
-    return "Hello, World!"
+if __name__ == "__main__":  # Correct use of __main_
+    app.run(host="0.0.0.0", port=5000, debug=True)  # Simplified port conversion
